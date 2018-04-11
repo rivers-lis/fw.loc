4d5f685a32eb65823916cca4d2019babbfc19129
rivers-lis <rivers86li@gmail.com>
2018-04-11 20:20:26 +0400

    Create class Route, pages 404

--- public/index.php
+++ public/index.php
@@ -5,14 +5,21 @@ $query = rtrim($_SERVER['QUERY_STRING'], '/');
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
 
-Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
-Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
+//Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
+//Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
+//Router::add('', ['controller' => 'Main', 'action' => 'index']);
+
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
+
+//Router::add('<controller:[a-z-]+>/<action[a-z-]+>');
 
 debug(Router::getRoutes());
 
-if (Router::matchRoute($query)) {
-    debug(Router::getRoute());
-}else{
-    echo '404';
-}
+Router::dispatch($query);
+
+//if (Router::matchRoute($query)) {
+//    debug(Router::getRoute());
+//}else{
+//    echo '404';
+//}
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -3,12 +3,26 @@
 
 class Router {
 
-//    public function __construct() {
-//        echo 'Привет, мир';
-//    }
+    /**
+     * таблица маршрута
+     * @var array
+     */
 
     protected static $routes = [];
+
+    /**
+     * текущий маршрут
+     * @var array
+     */
+
     protected static $route = [];
+
+    /**
+     * добавляет маршрут в таблицу маршрутов
+     *
+     * @param string $regexp регулярное вырожение маршрута
+     * @param array $route маршрут ([controller, action, params])
+     */
 
     public static function add($regexp , $route = []) {
         self::$routes[$regexp] = $route;
@@ -22,14 +36,30 @@ class Router {
         return self::$route;
     }
 
+    /**
+     * ищет URL в таблице маршрутов
+     * @param string $url входящий URL
+     * @return bool
+     */
+
     public static function matchRoute($url) {
         foreach (self::$routes as $pattern => $route) {
-            if ($url == $pattern) {
+            if (preg_match("#$pattern#i", $url, $matches)) {
+                debug($matches);
                 self::$route = $route;
                 return true;
             }
         }
         return false;
+    }
+
+    public static function dispatch($url) {
+        if (self::matchRoute($url)) {
+            echo 'OK';
+        }else{
+            http_response_code(404);
+            include '404.html';
+        }
     }
 
 }
